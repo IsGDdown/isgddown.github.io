7745f4a399a40b6fd98396965dfad1525647edd5
Kebab_Fan <155155186+Sylwester12345@users.noreply.github.com>
2026-09-18 15:37:56 +0200

    Update notification icon and badge paths

--- sw.js
+++ sw.js
@@ -9,8 +9,8 @@ self.addEventListener("push", event => {
   event.waitUntil(
     self.registration.showNotification(data.title, {
       body: data.body,
-      icon: "favicon.ico",
-      badge: "favicon.ico"
+      icon: "/favicon.ico",
+      badge: "/favicon.ico"
     })
   );
 });
